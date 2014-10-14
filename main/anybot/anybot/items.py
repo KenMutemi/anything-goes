0dcc3b3b4912841c026829e4579cd56d4f827d69
re <mutemikennedy@gmail.com>
2014-10-14 15:30:45 +0300

    Create first iteration of the index page

--- main/anybot/anybot/items.py
+++ main/anybot/anybot/items.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
+from scrapy.item import Item, Field
 import scrapy
-
 
 class AnybotItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
