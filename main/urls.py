042243af20b30b4ebba77a164c95835e4be42db1
re <mutemikennedy@gmail.com>
2014-10-19 00:33:39 +0300

    Now able to display title from submitted url

--- main/urls.py
+++ main/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from main import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),)
+        url(r'^$', views.index, name='index'),
+        url(r'^summary/$', views.summary, name='summary'),
+        )
--- main/views.py
+++ main/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from main.forms import URLForm
+from lxml import html
+import requests
 
 def index(request):
     if request.method == 'POST':
         url_form = URLForm(request.POST)
         if url_form.is_valid():
             cd = url_form.cleaned_data
-            request.session['url'] = cd['url']
+            url = cd['url']
+            response = requests.get(url)
+            tree = html.fromstring(response.text)
+            request.session['title'] = str(tree.xpath('//title/text()')[0]).replace('\\xa0', ' ')
+
+            return HttpResponseRedirect(reverse("summary"))
     else:
         url_form = URLForm(auto_id=False, label_suffix='')
     return render(request, 'main/index.html', {'url_form': url_form})
+
+def summary(request):
+    return render(request, 'main/summary.html', {'title': request.session['title']})
