85dabc3a73107f8c6ac3a88ec325a4a93121748d
re <mutemikennedy@gmail.com>
2014-10-19 03:04:20 +0300

    Remove anybot project and add js code to remove icon if image has error

--- main/anybot/anybot/items.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from scrapy.item import Item, Field
-import scrapy
-
-class AnyItem(scrapy.Item):
-    # define the fields for your item here like:
-    title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
