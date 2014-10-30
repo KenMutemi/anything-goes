7550ddd9ed8c834ae42dec5cb008e7a04f7432d3
re <mutemikennedy@gmail.com>
2014-10-30 15:59:55 +0300

    Add a delete summary button in summaries page

--- main/views.py
+++ main/views.py
@@ -33,8 +33,13 @@ def index(request):
                         summary.save()
 
             except (ValueError, Summary.DoesNotExist):
+                hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                       'Accept-Encoding': 'none',
+                       'Connection': 'keep-alive'}
                 try:
-                    response = requests.get(url)
+                    response = requests.get(url, headers=hdr)
                     parsed_uri = urlparse(url)
                     tree = html.fromstring(response.content)
                     try:
@@ -72,4 +77,8 @@ def summary(request):
 @login_required
 def summaries(request):
     url_form = URLForm(auto_id=False, label_suffix='')
+    if request.method == 'POST':
+        request.POST.get('delete')
+        summary = Summary.objects.get(url=request.POST.get("url", ""))
+        summary.user.remove(request.user.id)
     return render(request, 'main/summaries.html', {'url_form': url_form, 'summaries': Summary.objects.filter(user=request.user.id)})
