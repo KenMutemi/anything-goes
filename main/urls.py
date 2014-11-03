8ff0d7fcb5f4ab1216734a6efdca2ce23fcc3b00
re <mutemikennedy@gmail.com>
2014-11-03 20:08:27 +0300

    Separate web and image divs in summary page

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 from main import views
 
 urlpatterns = patterns('',
@@ -6,3 +7,9 @@ urlpatterns = patterns('',
         url(r'^summary/$', views.summary, name='summary'),
         url(r'^summaries/$', views.summaries, name='summaries'),
 )
+
+if settings.DEBUG:
+        # static files (images, css, javascript, etc.)
+            urlpatterns += patterns('',
+                            (r'^(?P<path>.*)$', 'django.views.static.serve', {
+                                        'document_root': settings.MEDIA_ROOT}))
