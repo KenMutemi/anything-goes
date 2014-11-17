f2fc0fb5c3d81aaaefa94b1d68d80a42f3b31015
root <root@tilder.ch>
2014-11-17 10:37:02 +0300

    Couple changes

--- main/tasks.py
+++ main/tasks.py
@@ -1,14 +1,13 @@
-from celery.task.schedules import crontab
 from celery.decorators import periodic_task
+from celery.task.schedules import crontab
 from main.utils.summary_update import update_summaries
 from celery.utils.log import get_task_logger
 from main.models import Summary
-summaries = Summary.objects.all()
 
+summaries = Summary.objects.all()
 logger = get_task_logger(__name__)
 
-# A periodic task that will run every minute (the symbol "*" means every)
-@periodic_task(run_every=(crontab(hour=21, minute=30, day_of_week="*")))
+@periodic_task(run_every=(crontab(hour=0, minute=30, day_of_week="*")))
 def summary_updater():
     for summary in summaries:
         logger.info("Start task")
