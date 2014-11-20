bccd5bb0a118d36e0544050e39e1d7db0cf10809
Kennedy Mutemi <kennedy@tilder.ch>
2014-11-20 16:45:49 +0300

    Change tasks.py and summaries_update

--- main/tasks.py
+++ main/tasks.py
@@ -2,14 +2,10 @@ from celery.decorators import periodic_task
 from celery.task.schedules import crontab
 from main.utils.summary_update import update_summaries
 from celery.utils.log import get_task_logger
-from main.models import Summary
 
-summaries = Summary.objects.all()
 logger = get_task_logger(__name__)
 
-@periodic_task(run_every=(crontab(hour=0, minute=30, day_of_week="*")))
+@periodic_task(run_every=(crontab(hour=10, minute=22, day_of_week="*")))
 def summary_updater():
-    for summary in summaries.iterator():
-        logger.info("Start task")
-        update_summaries(summary)
-        logger.info("Task finished: title = %s" % summary.title)
+    logger.info("Start task")
+    update_summaries()
--- main/utils/summary_update.py
+++ main/utils/summary_update.py
@@ -5,7 +5,9 @@ from main.helpers import remove_non_ascii
 import requests
 import lxml
 from lxml import html
+from main.models import Summary
 
+summaries = Summary.objects.all()
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -13,22 +15,24 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Accept-Encoding': 'none',
        'Connection': 'keep-alive'}
 
-def update_summaries(summary):
-    try:
-        response = requests.get(summary.url, headers=hdr)
-    except ConnectionError:
-        print "Could not connect to %s" % summary.url
-    tree = html.fromstring(response.content)
-    try:
-        summary.title = tree.xpath(remove_non_ascii('//title/text()'))[0].encode('utf-8').replace('Â', '')
-    except IndexError:
-        summary.title = '[title not available]'
-    for noscript in tree.xpath(".//noscript"):
-        noscript.getparent().remove(noscript)
-    paragraphs = tree.xpath('.//p')
-    summary.paragraphs = [remove_non_ascii(paragraph.text_content()) for paragraph in paragraphs]
-    parsed_uri = urlparse(summary.url)
-    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-    images = tree.xpath('.//img/@src')
-    summary.images = [domain+image if not image.startswith(domain) and not image.startswith('http') else image for image in images]
-    summary.save()
+def update_summaries():
+    for summary in summaries.iterator():
+        try:
+            response = requests.get(summary.url, headers=hdr)
+        except ConnectionError:
+            print "Could not connect to %s" % summary.url
+	    continue
+        tree = html.fromstring(response.content)
+        try:
+            summary.title = tree.xpath(remove_non_ascii('//title/text()'))[0].encode('utf-8').replace('Â', '')
+        except IndexError:
+            summary.title = '[title not available]'
+        for noscript in tree.xpath(".//noscript"):
+            noscript.getparent().remove(noscript)
+        paragraphs = tree.xpath('.//p')
+        summary.paragraphs = [remove_non_ascii(paragraph.text_content()) for paragraph in paragraphs]
+        parsed_uri = urlparse(summary.url)
+        domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+        images = tree.xpath('.//img/@src')
+        summary.images = [domain+image if not image.startswith(domain) and not image.startswith('http') else image for image in images]
+        summary.save()
